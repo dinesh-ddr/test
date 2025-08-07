15d14485dd922d63ecb71b4fe241b1143d12ff46
dinesh-ddr <drdmation@gmail.com>
2025-08-07 15:17:06 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int product = num1 * num2; // Perform multiplication
+
+        System.out.println("The product is: " + product); // Output the result
+    }
+}
